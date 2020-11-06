bdc02cbba7b0f4624084ba1899f2e182ca7d316b
SySfRaMe <32898648+SySfRaMe@users.noreply.github.com>
2020-11-06 19:15:55 +0100

    Update Idea1.sh

--- Idea1.sh
+++ Idea1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "installing required packages"
+echo "guide from: https://engineerworkshop.com/blog/raspberry-pi-vlan-how-to-connect-your-rpi-to-multiple-networks/"
 apt-get install vlan
 cp idea1_vlan /etc/network/interfaces.d/vlans
